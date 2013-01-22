ec817a31ae6b9e573790596d5466e532d6b96e88
Hasin Hayder <ironhide@leevio.com>
2013-01-22 21:13:08 +0600

    List Domains

--- lib/openshiftdomain.php
+++ lib/openshiftdomain.php
@@ -1,1 +1,17 @@
-openshiftdomain.php
+<?php
+
+class OpenShiftDomain{
+  private $endpoint = "https://openshift.redhat.com/broker/rest/domains";
+  private $auth;
+  private $dispatcher;
+
+  public __construct(OpenShiftAuth $auth, OpenShiftDispatcher $dispatcher){
+    $this->auth = $auth;
+    $this->dispatcher = $dispatcher;
+  }
+
+  public getDomains(){
+    $url = $this->endpoint;
+    $data = $this->dispatcher->get($url);
+  }
+}
