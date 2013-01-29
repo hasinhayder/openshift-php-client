8082641df650a54e9fafbbb8dbb9a8c530be7621
Hasin Hayder <ironhide@leevio.com>
2013-01-29 21:26:43 +0600

    OpenShiftDomains renamed to OpenShiftDomainManager for better understanding

--- lib/openshift.php
+++ lib/openshift.php
@@ -1,7 +1,7 @@
 <?php
 include_once("openshiftauth.php");
 include_once("openshiftdispatcher.php");
-include_once("openshiftdomains.php");
+include_once("openshiftdomainmanager.php");
 include_once("openshiftdomain.php");
 include_once("openshiftapp.php");
 include_once("openshiftcartridge.php");
@@ -10,6 +10,7 @@ include_once("objectbroker.php");
 class OpenShift{
   private $auth;
   private $dispatcher;
+  private $domainManager;
   public function __construct($username,$password){
     if(!$this->auth){
       $this->auth = new OpenShiftAuth($username,$password);
@@ -29,17 +30,10 @@ class OpenShift{
     return $this->dispatcher;
   }
 
-  public  function getDomains(){
-    if(!$this->domains){
-      $this->domains = new OpenShiftDomains();
+  public  function getDomainManager(){
+    if(!$this->domainManager){
+      $this->domainManager = new OpenShiftDomainManagaer();
     }
-    return $this->domains->getDomains();
-  }
-
-  public function getDomain($domainName){
-    if(!$this->domains){
-      $this->domains = new OpenShiftDomains();
-    }
-    return $this->domains->getDomain($domainName);
+    return $this->domainManager;
   }
 }
